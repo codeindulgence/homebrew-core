7fdc788cb2675b36ac33ad7a02d7447915b7475a
Nick Butler <nick@codeindulgence.com>
2018-09-03 22:03:54 +1000

    Formula styling

--- Formula/git-lock-branch.rb
+++ Formula/git-lock-branch.rb
@@ -1,11 +1,11 @@
 class GitLockBranch < Formula
-  desc "Protect your local branches from yourself."
+  desc "Protect your local branches from yourself"
   homepage "https://github.com/codeindulgence/git-lock-branch/"
   url "https://github.com/codeindulgence/git-lock-branch/archive/v1.0.tar.gz"
   sha256 "a09b0d3d3fae2087d72db7459305b92291141a90d32c9555cedca527866856de"
 
   def install
-    bin.install 'git-lock-branch'
+    bin.install "git-lock-branch"
   end
 
   test do
